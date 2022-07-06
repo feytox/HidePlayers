837431045cb612fc45b54ec8dc5c240ef01edb71
feytox <feytox@vk.com>
2022-07-06 21:05:33 +0500

    [v1.4]

---
+++ src/main/java/fr/minemobs/hideplayers/HidePlayersConfig.java
@@ -0,0 +1,9 @@
+package fr.minemobs.hideplayers;
+
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class HidePlayersConfig extends MidnightConfig {
+    @Entry
+    public static double distance = 64;
+}
