12e5c198aea1902923b6f4e556ede9ef6413a604
Feytox <feytox@vk.com>
2023-10-16 13:12:40 +0500

    Update to Minecraft 1.20.2

--- src/main/java/ru/feytox/toomanyplayers/mixin/PlayerListEntryMixin.java
+++ src/main/java/ru/feytox/toomanyplayers/mixin/PlayerListEntryMixin.java
@@ -3,7 +3,7 @@ package ru.feytox.toomanyplayers.mixin;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.util.DefaultSkinHelper;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.util.SkinTextures;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,11 +14,11 @@ import ru.feytox.toomanyplayers.mixin.accessors.PlayerListEntryAccessor;
 @Mixin(PlayerListEntry.class)
 public abstract class PlayerListEntryMixin {
 
-    @Inject(method = "getSkinTexture", at = @At("HEAD"), cancellable = true)
-    public void onGetSkinTexture(CallbackInfoReturnable<Identifier> cir) {
+    @Inject(method = "getSkinTextures", at = @At("HEAD"), cancellable = true)
+    public void onGetSkinTextures(CallbackInfoReturnable<SkinTextures> cir) {
         GameProfile profile = ((PlayerListEntryAccessor) this).getProfile();
         if (TMPConfig.hideskinlist.contains(profile.getName())) {
-            cir.setReturnValue(DefaultSkinHelper.getTexture(profile.getId()));
+            cir.setReturnValue(DefaultSkinHelper.getSkinTextures(profile.getId()));
         }
     }
 }
